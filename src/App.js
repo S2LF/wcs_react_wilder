017b48ea9d8b1f49b907af29316d83306fe0033b
S2LF <sdeuhelf@gmail.com>
2020-11-12 14:26:30 +0100

    useState into useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 import "./App.css";
 import { CardRow, Container, Footer, Header, ShowButton } from "./styles/elements";
@@ -9,69 +9,86 @@ import { ReactComponent as PlusCircle } from "./icons/add-circle.svg";
 import { ReactComponent as MinusCircle } from "./icons/minus-circle.svg";
 
 
-function App() {
+const initialState = {
+  showAddForm: false,
+  successMessage: "",
+  wilders: [],
+};
 
-  const [wilders, setWilders] = useState([]);
-  const [showAddForm, setShowAddForm] = useState(false);
-  const [successMessage, setSuccessMessage] = useState("");
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_SHOW_ADD_FORM":
+      return { showAddForm: !state.showAddForm};
+    case "WILDER_ADDED":
+      return {
+        showAddForm: false,
+        successMessage: `The wilder ${action.newWilder.name} has been successfully added`,
+        wilders: [{ ...action.newWilder, justAdded: true}, ...action.wilders],
+      };
+    case "WILDERS_FETCH_SUCCESS":
+      return { ...state, wilders: action.wilders};
+    default:
+      return state;
+  }
+}
+
+function App() {
+  
+  const [state, dispatch] = useReducer(appReducer, initialState);
 
   const fetchWilders = async () => {
     try {
       const result = await axios("http://localhost:5000/api/wilders");
-      setWilders(result.data.result);
+      dispatch({
+        type: "WILDERS_FETCH_SUCCESS",
+        wilders: result.data.result,
+      })
     } catch (error) {
       console.log(error);
     }
   };
 
-
   useEffect(() => {
     fetchWilders();
   }, []);
-  const closeForm = () => setShowAddForm(false);
 
   return (
     <div className="App">
-        <Header>
-          <Container>
-              <h1>Wilders Book</h1>
-          </Container>
-        </Header>
+      <Header>
         <Container>
-          <ShowButton
-            onClick={() => setShowAddForm(!showAddForm)}
-          >
-            {showAddForm ? <MinusCircle/> : <PlusCircle/>}
-          </ShowButton>
-          {showAddForm ? (
-
-            <AddWilder
-            onSuccess={(newWilder) => {
-              closeForm();
-              setSuccessMessage(`The wilder ${newWilder.name} has been successfully added`);
-              setWilders([{ ...newWilder, justAdded: true}, ...wilders]);
-            }}
-            />
-            ) : (
-              successMessage !== "" && <Success>{successMessage}</Success>
-            )}
+            <h1>Wilders Book</h1>
         </Container>
-        <Container>
-          <h2>Wilder</h2>
-        </Container>
-        <CardRow>
-          {wilders.map((wilder) => (
-            <Wilder 
-              key={wilder._id} 
-              {...wilder}
-            />
-          ))}
-        </CardRow>
-        <Container>
-          <Footer>
-            <p>&copy; 2020 Wild Code School</p>
-          </Footer>
-        </Container>
+      </Header>
+      <Container>
+        <ShowButton onClick={() => dispatch({ type: "TOGGLE_SHOW_ADD_FORM" })}>
+          {state.showAddForm ? <MinusCircle/> : <PlusCircle/>}
+        </ShowButton>
+        {state.showAddForm ? (
+          <AddWilder
+          onSuccess={(newWilder) => {
+            dispatch({ type: "WILDER_ADDED", newWilder})
+          }}
+          />
+        ) : (
+          state.successMessage !== "" && <Success>{state.successMessage}</Success>
+        )}
+      </Container>
+      <Container>
+        <h2>Wilder</h2>
+      </Container>
+      <CardRow>
+        {state.wilders.map((wilder) => (
+          <Wilder 
+            key={wilder._id} 
+            {...wilder}
+          />
+        ))}
+      </CardRow>
+      <Container>
+        <Footer>
+          <p>&copy; 2020 Wild Code School</p>
+        </Footer>
+      </Container>
     </div>
   );
 }
